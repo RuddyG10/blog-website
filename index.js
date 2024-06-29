55f28534a39d0951506f76c6a8cc4465bee25649
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2024-06-29 15:45:58 -0400

    agregado bootstrap y nav bar

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+//obtener el nombre de archivo y directorio actual
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const port = 3000;
 const app = express();
+/*
+    middleware
+*/
 
+//servir archivos estaticos
+app.use(express.static(path.join(__dirname,"public")));
+app.use('/bootstrap',express.static(path.join(__dirname,'node_modules/bootstrap/dist')));
+
+/*
+    Rutas
+*/
 app.get('/',(req,res)=>{
     res.render("index.ejs");
 });
